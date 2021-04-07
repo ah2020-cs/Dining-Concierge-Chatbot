ae04a4ca23962554d81cc98535c01e67396c967c
ah2020-cs <81828176+ah2020-cs@users.noreply.github.com>
2021-04-06 20:59:25 -0400

    Update lambda0.py

--- Lambda/lambda0.py
+++ Lambda/lambda0.py
@@ -1,34 +1,90 @@
-import boto3
 import json
-
-client = boto3.client('lex-runtime')
-
-def lambda_handler(event, context):
-	userMessage = event["message"]
-	userId = event["userId"]
-	botError = "Message Error"
-
-	if userMessage is None or len(userMessage) < 1:
-		return {'statusCode': 200,'body': json.dumps(botError)}
-
-	response = client.post_text(botName='DiningConciergeBot', botAlias='DCB', 
-		userId=userId, inputText=userMessage)
-
-	if response['message'] is not None or len(response['message']) > 0:
-		userMessage = response['message']
-
-	return {'statusCode': 200,'body': json.dumps(userMessage)}
-
+import boto3
+import time
+import os
 
 def lambda_handler(event, context):
-	user_id, text = get_info_from_request(event)
-	if user_id is None or text is None:
-	print("error")
-	chatbot_text = get_chatbot_response(user_id, text)
-	if chatbot_text is None:
-		return "Error"
-	else:
-		return get_success_response(chatbot_text, user_id)
+    user_id, text = get_info_from_request(event)
+    if user_id is None or text is None:
+        return get_error_response("Unable to get user ID and text from request")
+    chatbot_text = get_chatbot_response(user_id, text)
+    if chatbot_text is None:
+        return get_error_response("Fail to connect with lex")
+    else:
+        return get_success_response(chatbot_text, user_id)
+    
 
+def get_info_from_request(event):
+    body = event
+    if "messages" not in body:
+        return None, None
+    messages = event["messages"]
+    if not isinstance(messages,list) or len(messages) < 1:
+        return None, None
+    message = messages[0]
+    if "unconstructed" not in message:
+        return None, None
+    if "text" not in message["unconstructed"] or "user_id" not in message["unconstructed"]:
+        return None, None
+    user_id = message["unconstructed"]["user_id"]
+    text = message["unconstructed"]["text"]
+    return user_id, text
 
-
+def get_error_response(text):
+    body = {
+        "messages":[
+            {
+                "type":"responce message",
+                "unconstructed": {
+                    "user_id": None,
+                    "text": text,
+                    "time": time.time(),
+                }
+            }]
+    }
+    
+    response = {
+        "status code": 200,
+        "body": body
+    }
+    return response
+    
+def get_success_response(text,user_id):
+        
+    body = {
+        "messages":[
+            {
+                "type":"responce message",
+                "unconstructed": {
+                    "user_id": user_id,
+                    "text": text,
+                    "time": time.time()
+                }
+            }]
+    }
+    
+    
+    response = {
+        "status code": 200,
+        "body": body
+    }
+    return response
+    
+def get_chatbot_response(user_id,text):
+    message = ''
+    client = boto3.client('lex-runtime')
+    lex_response = client.post_text(
+        botName ='dining_concierge',
+        botAlias = 'dining_concierge',
+        userId = user_id,
+        inputText = text
+    )
+    
+    if not isinstance(lex_response, dict):
+        return None
+    
+    if 'message' not in lex_response:
+        return None
+        
+    message = lex_response['message']
+    return message
